b509ee152443fde1009f56bea5d2345e7291a4e2
j <no@notrightheckingnow.com>
2025-03-24 00:59:55 -0500

    little changes

--- lua/ide-tools.lua
+++ lua/ide-tools.lua
@@ -1,5 +1,6 @@
 local md = require('mini.deps')
 
+-- i forgot what stackoverflow answer i jacked this from lol
 function MergeTables(t1, t2)
 	for k, v in pairs(t2) do
 		if (type(v) == "table") and (type(t1[k] or false) == "table") then
@@ -12,7 +13,6 @@ function MergeTables(t1, t2)
 end
 
 md.setup({})
--- these five want to be a minor mode invoked by language based major modes
 -- Trouble gives me good error/lsp nav
 md.add({ source = 'https://github.com/folke/trouble.nvim.git' })
 -- Nvim DAP gives me the debug adapter protocol
@@ -51,10 +51,10 @@ local function map_keys()
 	NormalMapKey('<Leader>la', function() vim.lsp.buf.code_action() end,
 		"Selects a code action available at the current cursor position.")
 
-	NormalMapKey('<Leader>gD', function() vim.lsp.buf.declaration() end,
+	NormalMapKey('gD', function() vim.lsp.buf.declaration() end,
 		"Jumps to the declaration of the symbol under the cursor.")
 
-	NormalMapKey('<Leader>gd', function() vim.lsp.buf.declaration() end,
+	NormalMapKey('gd', function() vim.lsp.buf.definition() end,
 		"Jumps to the definition of the symbol under the cursor.")
 
 	NormalMapKey('<Leader>dd', function() trouble.open("diagnostics") end,
@@ -68,6 +68,29 @@ local function map_keys()
 
 	NormalMapKey('K', function() vim.lsp.buf.hover() end,
 		"Displays hover information about the symbol under the cursor.")
+
+	NormalMapKey('<Leader>lc', function() vim.lsp.buf.incoming_calls() end,
+		"Lists all the call sites of the symbol under the cursor")
+
+	NormalMapKey('<Leader>lC', function() vim.lsp.buf.outgoing_calls() end,
+		"Lists all the items that are called by the symbol under the cursor")
+
+	NormalMapKey('<Leader>lR', function() vim.lsp.buf.rename() end,
+		"Renames all references to the symbol under the cursor.")
+
+	NormalMapKey('<Leader>lr', function() vim.lsp.buf.references() end,
+		"Lists all the references to the symbol under the cursor")
+	NormalMapKey("<Leader>lt", function() vim.lsp.buf.signature_help() end,
+		"Displays signature information about the symbol under the cursor")
+
+	NormalMapKey("<Leader>lt", function() vim.lsp.buf.type_definition() end,
+		"Jumps to the definition of the type of the symbol under the cursor.")
+
+	NormalMapKey("<Leader>lt", function() vim.lsp.buf.typehierarchy("supertypes") end,
+		"Lists all the supertypes of the symbol under the cursor")
+
+	NormalMapKey("<Leader>lt", function() vim.lsp.buf.typehierarchy("subtypes") end,
+		"Lists all the subtypes of the symbol under the cursor")
 
 	wk.add({
 		{ "<leader>d", group = "󰑃 lsp/diagnostics" },
